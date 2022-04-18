19ad436f79e8593547ac110d10f092b3b27edb4e
MdRokibulHasan <mdrokibulhasan42@gmail.com>
2022-04-19 01:12:52 +0600

    added admin

--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -1,10 +1,17 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBxu1mR_57mykygStFqPQdHVIJcZpC41FI",
-  authDomain: "watches-online-6f326.firebaseapp.com",
-  projectId: "watches-online-6f326",
-  storageBucket: "watches-online-6f326.appspot.com",
-  messagingSenderId: "901767403852",
-  appId: "1:901767403852:web:ea332cbb65ff1495a617e9",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+
+  // apiKey: "AIzaSyBxu1mR_57mykygStFqPQdHVIJcZpC41FI",
+  // authDomain: "watches-online-6f326.firebaseapp.com",
+  // projectId: "watches-online-6f326",
+  // storageBucket: "watches-online-6f326.appspot.com",
+  // messagingSenderId: "901767403852",
+  // appId: "1:901767403852:web:ea332cbb65ff1495a617e9",
 };
 
 export default firebaseConfig;
